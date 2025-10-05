a039b823c605a53fdb32d02403a122e4e36b0719
Jason van Hattum <jason.vanhattum@fareharbor.com>
2025-10-05 18:36:24 +0200

    fix: adjust times
    Signed-off-by: Jason van Hattum <jason.vanhattum@fareharbor.com>

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     name: 'trevor',
     script: 'src/index.ts',
     interpreter: 'bun',
-    args: '--from 17:15 --to 18:45 --day tue --day wed --day thu',
+    args: '--from 17:15 --to 19:00 --day tue --day wed --day thu',
     cron_restart: '*/15 * * * *',
     autorestart: false,
     watch: false,
