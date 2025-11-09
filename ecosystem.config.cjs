179d331615f303d5f0f095a714fafd80aff12951
Jason van Hattum <jason.vanhattum@fareharbor.com>
2025-11-09 12:58:24 +0100

    chore: update days and times
    Signed-off-by: Jason van Hattum <jason.vanhattum@fareharbor.com>

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     name: 'trevor',
     script: 'src/index.ts',
     interpreter: 'bun',
-    args: '--from 17:30 --to 22:00 --day tue --day wed',
+    args: '--from 17:30 --to 20:00 --day tue --day wed',
     cron_restart: '*/5 * * * *',
     autorestart: false,
     watch: false,
